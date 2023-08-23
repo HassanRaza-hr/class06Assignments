0411328bc7d7c0d9cc5840ecc47bf4449ff77acd
Hassan Raza <141661553+HassanRaza-hr@users.noreply.github.com>
2023-08-23 19:10:02 +0500

    assignment3

---
+++ assign03.ts
@@ -0,0 +1,9 @@
+// Implement a program that uses a loop to iterate through an array of numbers and remove all the even numbers from them and just leave the odd ones.
+var ArrayOfNumbers:number[]=[1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<=ArrayOfNumbers.length;i++){
+    var odd=ArrayOfNumbers[i]
+    if(odd%2==0){
+        ArrayOfNumbers.splice(i,1)
+    }
+}
+console.log(ArrayOfNumbers)
