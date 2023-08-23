57fa17f1a1d52ded564289ab97bee276544dccc7
Hassan Raza <141661553+HassanRaza-hr@users.noreply.github.com>
2023-08-23 08:44:20 +0500

    assignment01 issue solved

--- assign01.js
+++ assign01.js
@@ -5,11 +5,20 @@
     console.log(sum)
 }*/ // failed
 // attempt #2
-var num = 20;
+var num = 19;
 let result = 0;
 for (let n = 0; n <= num; n++) {
     if (n % 2 == 0) {
         result += n;
     }
 }
-console.log(result);
+console.log(result); // here is the problem that i cannot tell the number of even numbers but i can tell number in range which add all even number til the input number
+let digit = 5;
+var result1 = 0;
+for (let n1 = 0; n1 <= digit * 2; n1++) {
+    if (n1 % 2 == 0) {
+        result1 += n1;
+    }
+    else { }
+}
+console.log(result1);
--- assign01.ts
+++ assign01.ts
@@ -4,7 +4,7 @@
     console.log(sum)
 }*/                                   // failed
 // attempt #2
-var num=20;
+var num=19;
 let result=0
 for(let n=0;n<=num ;n++){
     if(n%2==0){
@@ -12,4 +12,16 @@ for(let n=0;n<=num ;n++){
     }
    
 }
-console.log(result)
+console.log(result) // here is the problem that i cannot tell the number of even numbers but i can tell number in range which add all even number til the input number
+
+
+let digit=5;
+var result1=0;
+for(let n1=0;n1<=digit*2;n1++){
+    if(n1%2==0){
+        result1+=n1
+    }
+    else{}
+}
+console.log(result1)
+//now i can input the number of first even numbers
