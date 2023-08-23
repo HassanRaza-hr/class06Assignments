cf5299669bbdcc88d5dd5452d1f5c8d4165d259a
Hassan Raza <141661553+HassanRaza-hr@users.noreply.github.com>
2023-08-23 08:13:04 +0500

    first assignment of week 6 is solved

---
+++ assign01.js
@@ -0,0 +1,15 @@
+"use strict";
+// Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+/*for(let n=10;n>0 && n%2==0;n--){
+    var sum:number=n+n;
+    console.log(sum)
+}*/ // failed
+// attempt #2
+var num = 20;
+let result = 0;
+for (let n = 0; n <= num; n++) {
+    if (n % 2 == 0) {
+        result += n;
+    }
+}
+console.log(result);
